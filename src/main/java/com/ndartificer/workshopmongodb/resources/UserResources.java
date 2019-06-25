bfa117c9791b9dbad4e945afa524c1e40763ae00
NDArtificer <The.artificer@hotmail.com>
2019-06-25 17:58:09 -0300

    Retornando os Posts de um usuário

--- src/main/java/com/ndartificer/workshopmongodb/resources/UserResources.java
+++ src/main/java/com/ndartificer/workshopmongodb/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ndartificer.workshopmongodb.domain.Post;
 import com.ndartificer.workshopmongodb.domain.User;
 import com.ndartificer.workshopmongodb.dto.UserDTO;
 import com.ndartificer.workshopmongodb.services.UserService;
@@ -62,5 +63,11 @@ public class UserResources {
 		return ResponseEntity.noContent().build();
 		
 		
+	}
+	
+	@RequestMapping(value ="/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
