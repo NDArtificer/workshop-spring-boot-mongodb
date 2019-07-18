6bb2699c1cd0046948bbc81b9bd6dbc85a2f340c
NDArtificer <The.artificer@hotmail.com>
2019-07-18 15:11:16 -0300

    Acrescentando cometários aos posts

--- src/main/java/com/ndartificer/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/ndartificer/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ndartificer.workshopmongodb.domain.Post;
 import com.ndartificer.workshopmongodb.domain.User;
 import com.ndartificer.workshopmongodb.dto.AuthorDTO;
+import com.ndartificer.workshopmongodb.dto.CommentDTO;
 import com.ndartificer.workshopmongodb.repository.PostRepository;
 import com.ndartificer.workshopmongodb.repository.UserRepository;
 
@@ -41,6 +42,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Viajar para Olinda-PE, abraços! ", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia!", "Segundo dia em Olinda-PE, curtindo as férias! ", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem, meu amigo!", sdf.parse("21/07/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/07/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha uma ótimo dia!", sdf.parse("23/07/2018"), new AuthorDTO(alex));
+		
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
